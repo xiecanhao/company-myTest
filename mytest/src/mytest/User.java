58616467d6d33f45922cfe9ee8aa3d0ae4d3fcb2
xiecanhao <513693233@qq.com>
2017-10-23 17:02:02 +0800

    123456

--- mytest/src/mytest/User.java
+++ mytest/src/mytest/User.java
@@ -3,15 +3,12 @@ package mytest;
 public class User {
 
 	public User(int i, String string, int j) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public User(User guser) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public Integer getId() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
